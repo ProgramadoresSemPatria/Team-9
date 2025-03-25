b6d31f6c828409d978d5871a9cee92d8c695a5a0
Dnreikronos <dnreikronos2@gmail.com>
2025-03-25 19:27:58 -0300

    feature: create signin input struct

---
+++ backend/internal/models/user.go
@@ -0,0 +1,5 @@
+type SignInInput struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
