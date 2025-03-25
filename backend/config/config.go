d42406c9d6e43fff173b5f07022fba8581526787
Dnreikronos <dnreikronos2@gmail.com>
2025-03-24 22:25:26 -0300

    feature: create load configurations function

--- backend/config/config.go
+++ backend/config/config.go
@@ -21,3 +21,38 @@ func init() {
 	viper.SetDefault("database.database", "${POSTGRES_DATABASE}")
 	viper.SetDefault("database.timezone", "${POSTGRES_TIME_ZONE}")
 }
+
+func Load() error {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on environment variables")
+	}
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("../config")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	cfg = &config{
+		API: APIConfig{
+			Port: viper.GetString("api.port"),
+		},
+		DB: DBConfig{
+			Host:     viper.GetString("database.host"),
+			Port:     viper.GetString("database.port"),
+			User:     viper.GetString("database.user"),
+			Password: viper.GetString("database.pass"),
+			Database: viper.GetString("database.database"),
+			TimeZone: viper.GetString("database.timezone"),
+		},
+	}
+
+	log.Printf("Database config: %v", cfg.DB)
+	return nil
+}
+
