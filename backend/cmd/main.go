3f51079681ee7bbcab6b554d6c64493e787a4fcd
Dnreikronos <dnreikronos2@gmail.com>
2025-03-25 19:18:18 -0300

    feature: add gin gonic context in the database

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ProgramadoresSemPatria/Team-9/internal/config"
+	"github.com/ProgramadoresSemPatria/Team-9/internal/database/connection"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		panic(err)
 	}
+
 	err = config.Load()
 	if err != nil {
 		panic(fmt.Sprintf("Failed to load configuration: %v", err))
 	}
+
 	db, err := connection.OpenConnection()
 	if err != nil {
 		panic(err)
 	}
+
+	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%s", config.GetServerPort()), r)
+
+}
