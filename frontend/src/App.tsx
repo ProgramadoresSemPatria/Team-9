c0920049bdee03638c18d26f3953f5e759f0f778
Kauê <kauevecchia@hotmail.com>
2025-03-25 19:33:24 -0300

    fix: resolve package.json merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { Header } from "./components/Header";
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/Home';
+import RegisterPage from './pages/Register';
+import HeaderLayout from './layouts/Header';
+import SignInPage from './pages/SignIn';
 
 function App() {
     return (
         <>
-           <Header />
-        </>  
+            <Routes>
+                <Route element={<HeaderLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                </Route>
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="sign-in" element={<SignInPage />} />
+            </Routes>
+        </>
     );
 }
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,17 @@
-import menuSvg from '../assets/menu.svg'
-import userSvg from '../assets/user.svg'
-import arrowLeft from '../assets/arrow-left.svg'
+import menuSvg from '../assets/menu.svg';
+import userSvg from '../assets/user.svg';
+import arrowLeft from '../assets/arrow-left.svg';
 
-export function Header() {
+export const Header = () => {
     return (
         <header className="flex items-center justify-between p-8">
             <button>
-                <img src={menuSvg} alt="" />
+                <img src={menuSvg} alt="menu" />
             </button>
             <p className="text-lg font-bold">GoFit</p>
             <button>
-                <img src={userSvg} alt="" />
+                <img src={userSvg} alt="user" />
             </button>
         </header>
     );
-}
+};
