bea9f96faf8b4beede6473cdbeb8dccdad2729d8
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-25 07:59:28 -0300

    feat: create home, sign in and register route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import { Header } from './components/Header';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/Home';
+import RegisterPage from './pages/Register';
+import SignInPage from './pages/SignIn';
+import HeaderLayout from './layouts/Header';
 
 function App() {
     return (
         <>
-            <Header />
+            <Routes>
+                <Route element={<HeaderLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                </Route>
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="sign-in" element={<SignInPage />} />
+            </Routes>
         </>
     );
 }
