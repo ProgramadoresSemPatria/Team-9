486411d5388cceaaf84b36ccf448e7351f170673
Dnreikronos <dnreikronos2@gmail.com>
2025-03-25 19:28:09 -0300

    feature: create user reponse struct

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -3,3 +3,11 @@ type SignInInput struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+type UserResponse struct {
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Verified  bool      `json:"verified"`
+}
