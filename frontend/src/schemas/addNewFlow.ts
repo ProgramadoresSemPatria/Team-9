1f300d9069af376d24baaf71f3c89c2dfa7b8d59
Kauê <kauevecchia@hotmail.com>
2025-03-25 20:48:40 -0300

    feat: create addNewFlow schema validation using Zod

---
+++ frontend/src/schemas/addNewFlow.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod'
+
+export const addNewFlowSchema = z.object({
+    title: z.string().trim().min(1, "The title must have at least 1 character").max(50, "The title can not exceed 50 characters"),
+    workoutLevel: z.enum(["beginner", "intermediate", "advanced"]),
+    cover: z.string().optional()
+})
