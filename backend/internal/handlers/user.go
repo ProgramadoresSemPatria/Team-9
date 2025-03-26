4b3b059d8b0ed4c0ef89cfe92985697e05d9f81a
Dnreikronos <dnreikronos2@gmail.com>
2025-03-25 22:13:49 -0300

    feature: create hash password function

--- backend/internal/handlers/user.go
+++ backend/internal/handlers/user.go
@@ -1,1 +1,5 @@
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
