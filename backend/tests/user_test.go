f114ebfd0053c6953f035b0e567420a9ae6e0584
Dnreikronos <dnreikronos2@gmail.com>
2025-03-25 22:15:28 -0300

    tests: create tests for test create user hand;er

--- backend/tests/user_test.go
+++ backend/tests/user_test.go
@@ -11,3 +11,21 @@ func setupRouter() (*gin.Engine, *gorm.DB) {
 	router.GET("/profile", handlers.AuthMiddleware(), handlers.ProfileHandler)
 	return router, db
 }
+func TestCreateUserHandler(t *testing.T) {
+	router, db := setupRouter()
+	defer db.Exec("DROP TABLE users")
+
+	userInput := models.SignInInput{Name: "Teste" , Email: "test@example.com", Password: "password123"}
+	body, _ := json.Marshal(userInput)
+
+	req, _ := http.NewRequest("POST", "/create", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	var user models.User
+	db.Where("email = ?", userInput.Email).First(&user)
+	assert.NotEmpty(t, user.ID)
+	assert.True(t, user.Verified)
+}
