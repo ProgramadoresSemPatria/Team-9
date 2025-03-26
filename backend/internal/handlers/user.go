83b55a6e898d42b06e6af97a9abde52ab27a5872
Dnreikronos <dnreikronos2@gmail.com>
2025-03-25 22:13:58 -0300

    feature: create verify function

--- backend/internal/handlers/user.go
+++ backend/internal/handlers/user.go
@@ -3,3 +3,6 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+func verifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
