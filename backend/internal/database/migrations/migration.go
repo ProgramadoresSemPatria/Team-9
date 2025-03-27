a5473bebdd3659e37679fa96dadc7a84d9b94655
Dnreikronos <dnreikronos2@gmail.com>
2025-03-26 22:03:12 -0300

    feature: add migrations for the flows

--- backend/internal/database/migrations/migration.go
+++ backend/internal/database/migrations/migration.go
@@ -5,11 +5,11 @@ import (
 	"gorm.io/gorm"
 )
 
-
 func RunMigrations(db *gorm.DB) {
 	createTables(db)
 }
 
 func createTables(db *gorm.DB) {
 	db.AutoMigrate(&models.User{})
+	db.AutoMigrate((&models.Flow{}))
 }
