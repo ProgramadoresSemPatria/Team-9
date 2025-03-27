9f4c759c97c02a9077dc9b173029cebe2f557098
Dnreikronos <dnreikronos2@gmail.com>
2025-03-27 20:19:01 -0300

    feature: create before create workou day

--- backend/internal/models/train_day.go
+++ backend/internal/models/train_day.go
@@ -1,3 +1,12 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type WorkoutDay struct {
 	ID        uuid.UUID `json:"id,omitempty"`
 	Title     string    `json:"title"`
@@ -10,8 +19,14 @@ type WorkoutDay struct {
 	FlowID    uuid.UUID `json:"flow_id" gorm:"type:uuid;not null;index"`
 	Flow      Flow      `json:"flow" gorm:"foreignKey:FlowID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
+
 type WorkouDayInput struct {
 	Title    string `json:"title"`
 	Day      string `json:"day"`
 	Duration string `json:"duration"`
+}
+
+func (w *WorkoutDay) BeforeCreate(d *gorm.DB) (err error) {
+	w.ID = uuid.New()
+	return
 }
