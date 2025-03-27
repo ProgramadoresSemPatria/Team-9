e5ff347b1f8a3576402eb33bf8d918b784e8f428
Dnreikronos <dnreikronos2@gmail.com>
2025-03-26 22:02:54 -0300

    feature: create before create for flows

--- backend/internal/models/flow.go
+++ backend/internal/models/flow.go
@@ -1,3 +1,10 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Flow struct {
 	ID     uuid.UUID `json:"id,omitempty" gorm:"type:uuid;primary_key;"`
 	Title  string    `json:"title"`
@@ -6,8 +13,14 @@ type Flow struct {
 	UserID uuid.UUID `json:"user_id" gorm:"type:uuid"`
 	User   User      `json:"user" gorm:"foreignKey:UserID;references:ID"`
 }
+
 type FlowInput struct {
 	Title string `json:"title" binding:"required"`
 	Level string `json:"level" binding:"required"`
 	Cover string `json:"cover"`
+}
+
+func (f *Flow) BeforeCreate(d *gorm.DB) (err error) {
+	f.ID = uuid.New()
+	return
 }
