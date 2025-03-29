b2d1d94efe2d2ea08f7edef7c542946e214080d6
Kauê <kauevecchia@hotmail.com>
2025-03-29 20:00:10 -0300

    style: add cursor pointer to button and min-h-screen to container div

--- frontend/src/pages/WorkoutInfo.tsx
+++ frontend/src/pages/WorkoutInfo.tsx
@@ -20,7 +20,7 @@ const WorkoutInfo = () => {
     };
 
     return (
-        <div className="flex flex-col gap-5 p-8">
+        <div className="flex min-h-screen flex-col gap-5 p-8">
             <div className="flex flex-col gap-1.5">
                 <h2 className="text-lg font-bold">{workout.name}</h2>
                 <div className="flex gap-3">
@@ -48,7 +48,7 @@ const WorkoutInfo = () => {
                 ))}
             </div>
             <button
-                className="flex w-full items-center justify-center gap-1 rounded-md bg-black px-4 py-2 font-medium text-white"
+                className="flex w-full cursor-pointer items-center justify-center gap-1 rounded-md bg-black px-4 py-2 font-medium text-white"
                 onClick={handleFinishWorkout}
             >
                 Mark as finished
