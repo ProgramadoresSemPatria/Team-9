7b0efc21195ab57df8fbb83a7646845285f78e10
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-29 17:25:33 -0300

    feat: add types for user registration, login, and training

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -19,3 +19,36 @@ export type Flow = {
     title: string;
     level: string;
 };
+
+export type RegisterUser = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+export type Login = {
+    email: string;
+    password: string;
+};
+
+export type CreateExercise = {
+    trainingDayId: string;
+    title: string;
+    muscle: string;
+    repetitions: number;
+    sets: number;
+};
+
+export type CreateTrainingDay = {
+    flowId: string;
+    title: string;
+    dayOfWeek: string;
+    exercises: Exercise[];
+    duration: number;
+};
+
+export type CreateFlow = {
+    userId: string;
+    title: string;
+    level: string;
+};
