16a241544027a1061eb06236cc43181fe67f1ae6
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-28 21:41:46 -0300

    feat: implement search functionality in HomePage component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import searchIcon from '../assets/Search.svg';
 import FlowItem from '../components/FlowItem';
 import TrainingDayContainer from '../components/TrainingDayContainer';
@@ -10,7 +11,7 @@ const trainingDay: TrainingDay = {
     duration: 45,
 };
 
-const flows: Flow[] = [
+const flowsMock: Flow[] = [
     {
         id: '1',
         title: 'Aesthetic project',
@@ -39,6 +40,19 @@ const flows: Flow[] = [
 ];
 
 const HomePage = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [flowsFounded, setFlowsFounded] = useState<Flow[]>();
+
+    const handleSearchFlow = () => {
+        const flows = flowsMock.filter((flow) =>
+            flow.title.toLowerCase().includes(inputValue.toLowerCase())
+        );
+        setFlowsFounded(flows);
+        setInputValue('');
+    };
+
+    console.log(flowsFounded);
+
     return (
         <>
             <div className="flex w-full flex-col items-center">
@@ -46,9 +60,14 @@ const HomePage = () => {
                     <input
                         type="text"
                         placeholder="Search for flows..."
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
                         className="h-10 w-72 rounded-md border py-2.5 pl-3"
                     />
-                    <button className="cursor-pointer rounded-md border-transparent bg-black px-3 py-3 shadow-sm transition-colors duration-200 hover:bg-gray-800 focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 focus:outline-none">
+                    <button
+                        onClick={handleSearchFlow}
+                        className="cursor-pointer rounded-md border-transparent bg-black px-3 py-3 shadow-sm transition-colors duration-200 hover:bg-gray-800 focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 focus:outline-none"
+                    >
                         <img src={searchIcon} alt="search" />
                     </button>
                 </div>
@@ -60,8 +79,8 @@ const HomePage = () => {
                 <div className="flex w-full flex-col gap-2.5 p-8 pb-7">
                     <span className="text-xl font-bold">Your flows</span>
                     <div className="flex w-full gap-4 overflow-x-auto">
-                        {flows.length > 0 ? (
-                            flows.map((flow) => (
+                        {flowsMock.length > 0 ? (
+                            flowsMock.map((flow) => (
                                 <FlowItem key={flow.id} flow={flow} />
                             ))
                         ) : (
