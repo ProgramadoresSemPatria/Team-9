ddc615ddb7b10f6d0b733952e3bc0605f8300f75
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-29 23:31:25 -0300

    feat: update createTrainingDay to include flowId and convert duration to string

--- frontend/src/services/trainingDay/create.ts
+++ frontend/src/services/trainingDay/create.ts
@@ -3,14 +3,23 @@ import { CreateTrainingDay } from '../../types';
 
 const createTrainingDay = async (
     createTrainingDayParams: CreateTrainingDay,
+    flowId: string,
     token: string
 ) => {
     try {
-        const response = await api.post(`/workout-days`, createTrainingDayParams, {
-            headers: {
-                Authorization: `Bearer ${token}`,
+        const response = await api.post(
+            `/flows/${flowId}/workout-days`,
+            {
+                ...createTrainingDayParams,
+                duration: createTrainingDayParams.duration.toString(),
             },
-        });
+            {
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
 
         return response;
     } catch (error) {
