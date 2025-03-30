ff2c5160191f9aea49e281c9a52a0c740986ea56
Dnreikronos <dnreikronos2@gmail.com>
2025-03-30 18:42:23 -0300

    refactor: remove min and maximum size

--- backend/internal/models/exercise.go
+++ backend/internal/models/exercise.go
@@ -11,8 +11,8 @@ type Exercise struct {
 	ID           uuid.UUID  `json:"id" gorm:"type:uuid;primary_key;"`
 	Title        string     `json:"title" gorm:"size:255;not null"`
 	MuscleGroup  string     `json:"muscle_group" gorm:"size:255;not null"`
-	Repetitions  int64      `json:"repetitions" gorm:"size:255;not null"`
-	Sets         int64      `json:"sets" gorm:"size:255;not null"`
+	Repetitions  int64      `json:"repetitions" gorm:"not null"`
+	Sets         int64      `json:"sets" gorm:"not null"`
 	WorkoutDayID uuid.UUID  `json:"workout_day_id" gorm:"type:uuid;not null;index"`
 	WorkoutDay   WorkoutDay `json:"workout_day" gorm:"foreignKey:WorkoutDayID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	UserID       uuid.UUID  `json:"user_id" gorm:"type:uuid;not null;index"`
@@ -24,8 +24,8 @@ type Exercise struct {
 type ExerciseInput struct {
 	Title       string `json:"title" binding:"required,min=3,max=255"`
 	MuscleGroup string `json:"muscle_group" binding:"required,min=3,max=255"`
-	Repetitions int64  `json:"repetitions" binding:"required,min=3,max=255"`
-	Sets        int64  `json:"sets" binding:"required,min=3,max=255"`
+	Repetitions int64  `json:"repetitions" binding:"required"`
+	Sets        int64  `json:"sets" binding:"required"`
 }
 
 type ExerciseuUpdateInput struct {
