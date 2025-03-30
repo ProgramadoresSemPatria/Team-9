c37020d348bfdc9bc8a4876cb8f8636f3c8591ef
Kauê <kauevecchia@hotmail.com>
2025-03-30 16:51:13 -0300

    style: add hover effect to TrainingDayContainer component

--- frontend/src/components/TrainingDayContainer.tsx
+++ frontend/src/components/TrainingDayContainer.tsx
@@ -9,7 +9,7 @@ type TrainingDayContainerProps = {
 
 const TrainingDayContainer = ({ trainingDay }: TrainingDayContainerProps) => {
     return (
-        <div className="w-80 rounded-tl-md rounded-tr-4xl rounded-br-md rounded-bl-4xl bg-gradient-to-br from-red-500 to-purple-500 p-0.5 md:w-md">
+        <div className="w-80 rounded-tl-md rounded-tr-4xl rounded-br-md rounded-bl-4xl bg-gradient-to-br from-red-500 to-purple-500 p-0.5 transition-all hover:scale-105 hover:rotate-1 hover:duration-500 md:w-md">
             <div
                 key={trainingDay.id}
                 className="flex h-44 flex-col justify-end gap-1 rounded-tl-md rounded-tr-4xl rounded-br-md rounded-bl-4xl bg-white p-5"
