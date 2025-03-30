6864b2a0618623c834af6d98da4aa1a3203b8c40
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-29 23:10:07 -0300

    refactor: rename dayOfWeek to day in training day schema and change duration type to string

--- frontend/src/schemas/trainingDay.ts
+++ frontend/src/schemas/trainingDay.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const createNewTrainingDaySchema = z.object({
     title: z.string().min(1, 'Title is required'),
-    dayOfWeek: z.enum([
+    day: z.enum([
         'Monday',
         'Tuesday',
         'Wednesday',
@@ -11,5 +11,5 @@ export const createNewTrainingDaySchema = z.object({
         'Saturday',
         'Sunday',
     ]),
-    duration: z.number().int().positive('Duration must be a positive integer'),
+    duration: z.string(),
 });
