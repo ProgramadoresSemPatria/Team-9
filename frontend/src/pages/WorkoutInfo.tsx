d96e35924128842be0f5f5775e92c9a15b7c2fa1
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-31 21:15:02 -0300

    feat: add delete functionality for training day with confirmation button

--- frontend/src/pages/WorkoutInfo.tsx
+++ frontend/src/pages/WorkoutInfo.tsx
@@ -1,25 +1,28 @@
-import { Fire, Timer } from 'phosphor-react';
+import { Fire, Timer, Trash } from 'phosphor-react';
 import ExerciseInfo from '../components/ExerciseInfo';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
 import { Exercise, TrainingDay } from '../types';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import getExercises from '../services/exercises/getAll';
 import getTrainingDayById from '../services/trainingDay/getById';
+import deleteTrainingDay from '../services/trainingDay/delete';
 
 const WorkoutInfo = () => {
+    const [trainingDay, setTrainingDay] = useState<TrainingDay>();
+    const [exercises, setExercises] = useState<Exercise[]>([]);
+
+    const navigate = useNavigate();
+
     const { id } = useParams();
 
     if (!id) return;
 
-    const [trainingDay, setTrainingDay] = useState<TrainingDay>();
-    const [exercises, setExercises] = useState<Exercise[]>([]);
+    const token = Cookies.get('auth_token');
+
+    if (!token) throw new Error('JWT token invalid');
 
     useEffect(() => {
-        const token = Cookies.get('auth_token');
-
-        if (!token) throw new Error('JWT token invalid');
-
         const getTrainingDay = async () => {
             const response = await getTrainingDayById(id, token);
 
@@ -43,10 +46,30 @@ const WorkoutInfo = () => {
         getTrainingDay();
     }, []);
 
+    const handleDeleteWorkout = async () => {
+        if (!trainingDay?.id) return;
+
+        const response = await deleteTrainingDay(trainingDay.id, token);
+
+        if (response?.status !== 200) {
+            throw new Error('Error to delete training day');
+        }
+
+        navigate(-1);
+    };
+
     return (
         <div className="mx-auto flex min-h-screen max-w-4xl flex-col gap-5 p-8">
             <div className="flex flex-col gap-1.5">
-                <h2 className="text-lg font-bold">{trainingDay?.title}</h2>
+                <div className="flex items-center justify-between">
+                    <h2 className="text-lg font-bold">{trainingDay?.title}</h2>
+                    <button
+                        onClick={handleDeleteWorkout}
+                        className="cursor-pointer rounded-md bg-red-700 p-2 text-white transition duration-200 hover:bg-red-800"
+                    >
+                        <Trash size={20} />
+                    </button>
+                </div>
                 <div className="flex gap-3">
                     <div>
                         <span className="flex items-center gap-1 text-gray-400">
